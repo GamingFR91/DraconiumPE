8e477681fa94c3cfe215f1684fdebd90eaaef11f
GamingFR91 <52374013+GamingFR91@users.noreply.github.com>
2019-12-16 13:23:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,13 +24,13 @@ bot.on("message", function(message) {
 // Commands.
     if (command == "test") {
         var embedhelpmember = new Discord.RichEmbed()
-        this.emitter.emit('full_stat', {
+        
             .setAuthor("Info Serveur")
             .addField("Joueurs: kv[15]/kv[17]")
             .addField(" - ping", "PING PONG.")
             .setColor(0x00FFEE)
             .setFooter("Ⓒ 2019 Example Bot.", bot.user.displayAvatarURL);
-            });
+            
             message.channel.send(embedhelpmember)
         if(message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(embedhelpadmin);
     };
