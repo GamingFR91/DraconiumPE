49a22668838241dd087a480b91ad59d934797fe3
GamingFR91 <52374013+GamingFR91@users.noreply.github.com>
2020-03-09 17:03:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -106,6 +106,7 @@ bot.on("message", function(message) {
         const DiscordServ = bot.guilds.get(message.guild.id)
         message.channel.send(DiscordServ.memberCount);
         message.channel.send(DiscordServ.owner.user.tag);
+        message.channel.send(bot.ping);
     };
 
     if(command === "clear") {
