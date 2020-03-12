c83e8eff1cfa2300f109c24189570c9991c840c6
GamingFR91 <52374013+GamingFR91@users.noreply.github.com>
2020-03-12 17:28:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const Query = require("minecraft-query");
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.login("NjU3MTQ5ODM4MjUyODM0ODE2.XmiqgA.G3RYZrgZPnlIMYlL5G3vUViVd4Q");
 
-const q = new Query({host: 'draconium.ouimc.fr', port: 25561, timeout: 7500});
+var request = require('request');
+var mcCommand = 't!ip'; // Command for triggering
+var mcIP = 'sulfuritium.mcpe.dev'; // Your MC server IP or hostname address
+var mcPort = 19132; // Your MC server port (25565 is the default)
 
-q.fullStat()
-  .then(success => {
+// IMPORTANT: You need to run "npm install request" (without quotes) in your terminal before executing this script
 
-    console.log(success);
-
-    return q.basicStat()
-
-  })
-
-  .then(success => {
-
-    console.log(success);
-
-    q.close();
-
-  })
+client.on('message', message => {
+    if (message.content === mcCommand) {
+        var url = 'http://mcapi.us/server/query?ip=' + mcIP + '&port=' + mcPort;
+        request(url, function(err, response, body) {
+            if(err) {
+                console.log(err);
+                return message.channel.send('Une erreur a eu lieu pendant la récuperation du status du serveur');
+            }
+            body = JSON.parse(body);
+            var status = '**Sulfuritium**\nIP: sulfuritium.mcpe.dev\nPort: 19132\nStatus: Hors Ligne';
+            if(body.online) {
+                status = '**Sulfuritium**\nIP: sulfuritium.mcpe.dev\nPort: 19132\nStatus: En Ligne\nConnectés: ' + body.players.now + '/' + body.players.max;
+              
+            }
+            message.channel.send(status);
+        });
+    }
+});
